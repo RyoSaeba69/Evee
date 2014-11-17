e8a63cb101b7d16c11856e5c79dc304f6a25c0b6
Jean-François <jf67.muller@gmail.com>
2014-11-17 18:47:24 +0100

    ajout de la modalConnection

---
+++ public/user/service/modalConnectionService.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jean-François on 17/11/2014.
+ */
+angular.module('evee.user')
+    .factory('modalConnectionService', ["$modal", function ($modal) {
+        return {
+            open: function () {
+                return $modal.open({
+                    templateUrl: "user/templates/modalConnection.html",
+                    controller: "modalConnection",
+                    size: 'lg'
+                });
+            }
+        };
+    }]);
