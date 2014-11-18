4c14a82db545cfb21985383ad1c17b211d7d90b3
Antoine Muller <nemak121@gmail.com>
2014-11-18 09:11:19 +0100

    Reorganizing server side files. Fixes bootstrap bug when gulp.

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 
 var app = express();
@@ -21,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', routes);
 
