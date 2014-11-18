5c1a7918fa3aa57235829978515a9c839fcd1558
Jean-François <jf67.muller@gmail.com>
2014-11-18 15:02:22 +0100

    modification de la modalConnection

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ var app = express();
 //app.set('views', path.join(__dirname, 'public'));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '../public/ressources/images/logo.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
