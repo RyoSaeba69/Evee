8a017643672ebc956c5ace71e0f821d9046d10c7
Quentin <mrtchiki@gmail.com>
2015-01-20 15:46:23 +0100

    Add wishList directive

---
+++ public/event/directives/wishList.js
@@ -0,0 +1,14 @@
+/**
+ * Created by quentin on 19/01/15.
+ */
+angular.module('event')
+    .directive('wishList', function(){
+        return{
+            restrict:'E',
+            templateUrl:'event/templates/wishList.html',
+            link:function(scope){
+
+            }
+
+        }
+    });
