f76c1e1d12acbc973c37f6892fc10bc50546d922
Antoine Muller <nemak121@gmail.com>
2015-01-27 14:58:57 +0100

    add module when creating dispevent

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-Evee
-====
+# Evee
 
-L’objectif de notre projet est de réaliser une application web qui permettrait de faciliter l’organisation de différents évènements (anniversaires, fête, etc...). Le site devra être accessible à toute personne qu’ils soit inscrit ou non au site.
+a [Sails](http://sailsjs.org) application
---
+++ assets/event/directives/addEventClick.js
@@ -0,0 +1,52 @@
+/**
+ * Created by antoine on 1/21/15.
+ */
+
+angular.module('evee.event')
+    .directive('addEventClick', ['addEventModal', 'authService', '$state', 'eveeHttp', 'eveeLoader',
+        function (addEventModal, authService, $state, eveeHttp, eveeLoader) {
+            return {
+                restrict: 'A',
+                link: function (scope, element) {
+
+
+                    element.on('click', function () {
+
+                        // TODO: Uncomment when not dev
+                        //if(!authService.isLogged()){
+                        //    return;
+                        //}
+
+                        addEventModal.open()
+                            .result.then(function (newEvent) {
+                                eveeLoader.show();
+                                newEvent.modules = [
+                                    {name: 'testModule1', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule2', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule3', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule4', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule5', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule6', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule7', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule8', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule9', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule10', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule11', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule12', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule13', directive: {name:'hello-module', options: {foo: 'Bar'}}},
+                                    {name: 'testModule14', directive: {name:'hello-module', options: {foo: 'Bar'}}}
+                                ];
+                                console.log('NEW EVENT to save', newEvent);
+                                eveeHttp.post('events/save', {
+                                    data: newEvent
+                                }).success(function (data) {
+                                    $state.go('event', {eventId: data._id});
+                                    eveeLoader.hide();
+                                });
+                            }, function () {
+                                console.log('CLOSED');
+                            });
+                    });
+                }
+            };
+        }]);
